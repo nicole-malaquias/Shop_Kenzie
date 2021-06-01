c703742265891e5ae23ba00f2b86b547e702e49a
Nicole-Malaquias <nicolemalaquias91@gmail.com>
2021-06-01 15:00:53 -0300

    refactor: ajustando para remover em tempo os itens do carrinho

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,23 +1,32 @@
 import { StrictMode } from "react";
 import { useSelector } from "react-redux";
-
+import React, { useState, useEffect } from "react";
 import Product from "../Product";
 import Menu from "../Menu ";
 
 import Container from "./style";
 const Cart = () => {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const [carrinho, setCarrinho] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
+
+  const cart = JSON.parse(localStorage.getItem("cart"));
+  useEffect(() => {
+    const temp = JSON.parse(localStorage.getItem("cart"));
+    setCarrinho(temp);
+  }, [cart]);
+
   return (
     <Container>
       <Menu />
       <div className="meu-carrinho">
         <h1> Meu carrinho de compras</h1>
       </div>
-      {cart.map((product) => (
+      {carrinho.map((product) => (
         <Product key={product.id} product={product} isRemovable />
       ))}
       <div className="total">
-        R$ {cart.reduce((acc, item) => (acc += item.price), 0)}
+        R$ {cart.reduce((acc, item) => (acc = item.price), 0)}
       </div>
     </Container>
   );
